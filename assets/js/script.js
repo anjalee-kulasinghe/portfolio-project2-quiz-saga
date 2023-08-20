f9833ef8a8a2bef87f1ee686248b802fa88b646c
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-08-20 22:05:55 +0000

    Add html user section and script for the greetings()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("connected");
+// Welcome message to the user with the username has entered by the user
+
+let userName = document.querySelector("#userName");
+let headingText = document.querySelector("#headingText");
+let userPage = document.querySelector("#userPage");
+/**
+ * Welcome message with the username user has enetered
+ */
+function greetings() {
+    headingText.innerHTML = "";
+    headingText.innerHTML = "Hello " + userName.value + ". Select the quize that you would like to play.";
+    userPage.style.display = "none";
+}
