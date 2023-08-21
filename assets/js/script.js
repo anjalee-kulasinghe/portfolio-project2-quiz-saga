fecffa582af0a8a240eed28d38b267e4c687d6b6
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-08-21 00:48:40 +0000

    Add code for score, timer and countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,16 +27,54 @@ let quizAnswer = document.querySelectorAll(".quizQuestion .quizAnswer input");
 
 skip.addEventListener("click", function(){
     quizStep();
+    duration == 10;
 })
+ /* let the score added. Each correct question get 20 marks*/
+quizAnswer.forEach(function(quizAnswerSingle){
+    quizAnswerSingle.addEventListener("click", function(){
+        setTimeout(function(){
+            quizStep(); 
+            duration == 10;
+        },500)
 
+        let valid = this.getAttribute("valid");
+        if(valid === "valid") {
+            scoreCount += 20;
+            score.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+        } else {
+            scoreCount -= 20;
+            score.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+        }
+    })
+});
+/**
+ * let the skip button works
+ */
 function quizStep() {
     count += 1;
-    for(i = 0; i < quizQuestion.length; i++) {
+    for(let i = 0; i < quizQuestion.length; i++) {
         quizQuestion[i].className = "quizQuestion";
     }
 
-    quizQuestion[i].className = "quizQuestion active";
+    quizQuestion[count].className = "quizQuestion active";
     if(count == 5) {
         skip.style.display = "none";
+        clearInterval(timeDuration);
+        countdown.innerHTML = 0;
     }
 }
+
+/* set the timer in the quiz window */
+let timeDuration = setInterval(function(){
+    if(duration == 10) {
+        duration = 0;
+    }
+
+    duration += 1;
+    countdown.innerHTML = duration;
+    if(countdown.innerHTML == "10") {
+        quizStep();
+    }
+},1000);
