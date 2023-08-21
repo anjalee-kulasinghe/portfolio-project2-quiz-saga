9826fc97b2d1d5f57d1fefa672cfb44694779ca6
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-08-21 00:12:49 +0000

    Add the quize to the html and style it

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,32 @@ function greetings() {
     headingText.innerHTML = "Hello " + userName.value + ". Select the quize that you would like to play.";
     userPage.style.display = "none";
 }
+
+/* Quiz Score */
+let skip = document.getElementById("skip");
+let score = document.getElementById("score");
+let totalScore = document.getElementById("totalScore");
+let countdown = document.getElementById("countdown");
+
+let count = 0;
+let scoreCount = 0;
+let duration = 0;
+
+let quizQuestion = document.querySelectorAll(".quizQuestion");
+let quizAnswer = document.querySelectorAll(".quizQuestion .quizAnswer input");
+
+skip.addEventListener("click", function(){
+    quizStep();
+})
+
+function quizStep() {
+    count += 1;
+    for(i = 0; i < quizQuestion.length; i++) {
+        quizQuestion[i].className = "quizQuestion";
+    }
+
+    quizQuestion[i].className = "quizQuestion active";
+    if(count == 5) {
+        skip.style.display = "none";
+    }
+}
