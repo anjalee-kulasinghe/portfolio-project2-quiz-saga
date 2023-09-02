6551c22d5831620a96e078bb989ca25d55bae017
Anjalee Kulasinghe <anjaleek@gmail.com>
2023-09-02 18:47:38 +0000

    Add the code to disply the welcome message with the user name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+let userName = document.querySelector("#userName");
+let headingText = document.querySelector("#headingText");
+let userPage = document.querySelector("#userPage");
+/**
+ * Welcome message with the username user has enetered
+ */
+function greetings() {
+    headingText.innerHTML = "";
+    headingText.innerHTML = "Hello " + userName.value + ". Select the 'Start the quiz' to play the quiz. Click the 'How to Play' to read the instructions.";
+    userPage.style.display = "none";
+}
+
 //Code for the Modal
 document.addEventListener("DOMContentLoaded", function() {
     let modal = document.getElementById("myModal");
